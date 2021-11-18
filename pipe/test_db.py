ceab1503f39666a322a80e70dbb487330bc906c2
Zhen3r <xzzxzz2010@163.com>
2021-11-18 12:32:29 -0500

    update the comment, removing unused lib

--- pipe/test_db.py
+++ pipe/test_db.py
@@ -1,14 +1,13 @@
-from os import environ
 import pandas as pd
 import psycopg2 as pg
 from dotenv import load_dotenv
-from pathlib import Path
 
+# loading the remote database info (hostname, pwd, port, etc.)
+# into environment variables.
 envFilePath = '/Users/zh3n/Penn/OneDrive - PennO365/class/musa509/final-proj-509/pipe/.env'
-
 load_dotenv(envFilePath)
 
-# The database info is in env vars, thus using empty str to access.
+# Passing empty str to use env vars to access db.
 engine = pg.connect("")
 
 df = pd.read_sql('select 1 as hello_world', con=engine)
