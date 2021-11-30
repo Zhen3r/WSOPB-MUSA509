d9c3a115af05f2710b89d0282e0c98ccf61d485a
Zhen3r <xzzxzz2010@163.com>
2021-11-29 20:27:37 -0500

    change init file to main

---
+++ dags/main.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+bash_command = "cd /home/zhen/code/musa509-final-proj && proxychains /home/zhen/conda/envs/509/bin/python -m dags.dataPipeline._"
+
+with DAG(dag_id='park_pipeline',
+         schedule_interval='@daily',
+         start_date=datetime(2021, 11, 29),
+         catchup=False) as dag:
+
+    # EXTRACT TASKS
+    downloadACS = BashOperator(
+        task_id='downloadACS',
+        bash_command=bash_command.replace("_", "downloadACS"),
+        dag=dag)
+
+    downloadParks = BashOperator(
+        task_id='downloadParks',
+        bash_command=bash_command.replace("_", "downloadParks"),
+        dag=dag)
+
+    # TRANSFORM TASKS
+    transformACS = BashOperator(
+        task_id='transformACS',
+        bash_command=bash_command.replace("_", "transformACS"),
+        dag=dag)
+
+# DEPENDENCIES
+downloadACS >> transformACS
+downloadParks >> transformACS
